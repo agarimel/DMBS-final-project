f4872cba6a35509fdae1d123c0825071f59da49f
Nsai-roshan <81345700+Nsai-roshan@users.noreply.github.com>
2023-12-04 21:56:20 -0700

    updated follow functionality

--- app.py
+++ app.py
@@ -20,6 +20,35 @@ conn = psycopg2.connect(
     password=DB_PASSWORD
 )
 
+
+def check_follow_status(follower_username, follower_email_id, followed_username, followed_email_id):
+    # Check if there is a record in the 'follow' table indicating that
+    # the follower is following the user with the given username and email
+    with conn.cursor() as cursor:
+        cursor.execute("""
+            SELECT * FROM follow
+            WHERE follower_username = %s AND follower_email_id = %s
+                AND followed_username = %s AND followed_email_id = %s
+        """, (follower_username, follower_email_id, followed_username, followed_email_id))
+        return cursor.fetchone() is not None
+
+def update_follow_status(follower_username, follower_email_id, followed_username, followed_email_id):
+    # Check if the follow relationship already exists
+    if not check_follow_status(follower_username, follower_email_id, followed_username, followed_email_id):
+        # If not, insert a new record into the 'follow' table
+        with conn.cursor() as cursor:
+            try:
+                cursor.execute("""
+                    INSERT INTO follow (follower_username, follower_Email_ID, followed_username, followed_Email_ID)
+                    VALUES (%s, %s, %s, %s)
+                """, (follower_username, follower_email_id, followed_username, followed_email_id))
+                conn.commit()
+            except Exception as e:
+                print(f"Error inserting into follow table: {e}")
+                conn.rollback()
+
+
+
 @app.route('/')
 def registration_page():
     return render_template('registration.html')
@@ -103,12 +132,24 @@ def login_page():
     
 @app.route('/dashboard/<username>')
 def dashboard_page(username):
-    return render_template('dashboard.html', username=username)
+    # Fetch users and their follow status from the database
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT username, Email_ID FROM \"User\"")
+        users = cursor.fetchall()
+
+    # Example: Fetch the follow status for a particular user (replace with your logic)
+    follow_status = {}
+    for user in users:
+        follow_status[user] = check_follow_status(user[0], user[1], "current_user_username", "current_user_email")
+
+    return render_template('dashboard.html', username=username, users=users, follow_status=follow_status)
 
 @app.route('/user/<username>/dashboard')
 def user_dashboard(username):
     # Render the user dashboard template
     return render_template('dashboard.html', username=username)
+
+
 
 @app.route('/success')
 def success_page():
@@ -118,6 +159,38 @@ def success_page():
 def logout():
     # Add any logout logic here if needed
     return redirect(url_for('login_page'))
+
+
+@app.route('/follow', methods=['POST'])
+def follow():
+    try:
+        # Get the form data
+        follower_username = request.form['follower_username']
+        followed_username = request.form['followed_username']
+
+        # Retrieve email from the User table based on the username
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                SELECT Email_ID FROM "User"
+                WHERE username = %s
+            """, (follower_username,))
+            follower_email = cursor.fetchone()[0]
+
+            cursor.execute("""
+                SELECT Email_ID FROM "User"
+                WHERE username = %s
+            """, (followed_username,))
+            followed_email = cursor.fetchone()[0]
+
+        # Add logic to update the follow status in the database (replace with your logic)
+        update_follow_status(follower_username, follower_email, followed_username, followed_email)
+
+        return redirect(url_for('dashboard_page', username=follower_username))
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
